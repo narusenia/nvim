01bc733f8eb21af9fef9a2a019786c3cb7ec2b7d
Naruse Nia <notshinsin0000@gmail.com>
2026-07-01 14:29:42 +0900

    chore: enable clipboard

--- init.lua
+++ init.lua
@@ -23,6 +23,8 @@ vim.opt.cursorline = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.opt.clipboard = "unnamedplus"
+
 -- Folding --
 vim.o.foldcolumn = "1" -- '0' is not bad
 vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
